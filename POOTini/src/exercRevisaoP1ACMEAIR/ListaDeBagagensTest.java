d326f9bddec57a3027d92c213d5a96d8fb2dc59c
Israel Deorce <israel.idvj@gmail.com>
2017-04-24 01:28:37 -0300

    Classe de testes implementada

--- POOTini/src/exercRevisaoP1ACMEAIR/ListaDeBagagensTest.java
+++ POOTini/src/exercRevisaoP1ACMEAIR/ListaDeBagagensTest.java
@@ -1,7 +1,36 @@
 package exercRevisaoP1ACMEAIR;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
 public class ListaDeBagagensTest {
 	
+	private ListaDeBagagens lista;
 	
+	@Before
+	public void setUp(){
+		lista = new ListaDeBagagens();
+		BagagemNormal.setCustoFixo(10);
+		lista.add(new BagagemNormal(22,"Bagagem Normal 1"));
+		lista.add(new BagagemFragil(23,"Bagagem Fragil 1"));
+		lista.add(new BagagemDeMao(10,"Bagagem de Mao 1"));		
+	}
+	
+	@Test
+	public void testGetTotalBagagensDeMao(){
+		lista.add(new BagagemDeMao(4,"Bagagem de Mao 2"));	
+		int expected = 2;
+		int actual = lista.getTotalBagagensDeMao();
+		assertEquals(expected, actual, 0.1);
+	}
+	
+	@Test
+	public void testCustoTotal(){
+		lista.custoTotal();
+		double expected = 130;
+		double actual = lista.custoTotal();
+		assertEquals(expected, actual, 0.1);
+	}
 
 }
